7a0389aed2fedccd3b000567f5efc7034b2c3cdd
Michael Ciabuca <ciabuca@gmail.com>
2020-07-24 22:24:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc(array)
+  array.sort { |a, b| a <=> b }
+end
+
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort_by { |x| x.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse()
+end
+
+def kesha_maker(array)
+  
+end
+
+
 
